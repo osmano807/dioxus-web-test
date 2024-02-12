7639d391999a21b1fa0d5a29a21ad8350d3f72c4
Joaquim Simão <osmano807@gmail.com>
2024-02-11 23:24:50 -0300

    style: reformat nix with alejandra

--- flake.nix
+++ flake.nix
@@ -14,34 +14,33 @@
   };
 
   outputs = inputs:
-    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+    inputs.flake-parts.lib.mkFlake {inherit inputs;} {
       systems = import inputs.systems;
       imports = [
         inputs.treefmt-nix.flakeModule
       ];
-      perSystem =
-        { config
-        , self'
-        , pkgs
-        , lib
-        , system
-        , ...
-        }:
-        let
-          cargoToml = builtins.fromTOML (builtins.readFile ./Cargo.toml);
-          nonRustDeps = [
-            pkgs.libiconv
-          ];
-          fenixPkgs = inputs.fenix.packages.${system};
+      perSystem = {
+        config,
+        self',
+        pkgs,
+        lib,
+        system,
+        ...
+      }: let
+        cargoToml = builtins.fromTOML (builtins.readFile ./Cargo.toml);
+        nonRustDeps = [
+          pkgs.libiconv
+        ];
+        fenixPkgs = inputs.fenix.packages.${system};
 
-          fenix-channel = fenixPkgs.latest;
-          # rust targets
-          fenix-targets = with fenixPkgs.targets; [
-            x86_64-unknown-linux-gnu.latest.rust-std
-            wasm32-unknown-unknown.latest.rust-std
-          ];
+        fenix-channel = fenixPkgs.latest;
+        # rust targets
+        fenix-targets = with fenixPkgs.targets; [
+          x86_64-unknown-linux-gnu.latest.rust-std
+          wasm32-unknown-unknown.latest.rust-std
+        ];
 
-          fenix-toolchain = fenixPkgs.combine ([
+        fenix-toolchain = fenixPkgs.combine ([
             fenix-channel.rustc
             fenix-channel.cargo
             fenix-channel.clippy
@@ -52,108 +51,96 @@
           ]
           ++ fenix-targets);
 
-          /* rust-toolchain = pkgs.symlinkJoin {
-            name = "rust-toolchain";
-            paths = with fenixPkgs.default; [
-              rustc
-              cargo
-              clippy
-              rustfmt
-              fenixPkgs.rust-analyzer
-              fenixPkgs.complete.rust-src
-              pkgs.nixd
-              pkgs.dioxus-cli # Should I use nixpkgs nightly or directly build from dioxus git?
-            ];
-          }; */
-          rust-toolchain = fenix-toolchain;
-          rustPlatform = pkgs.makeRustPlatform {
-            cargo = fenix-toolchain;
-            rustc = fenix-toolchain;
-          };
-        in
-        {
-          # Rust package
-          packages.default = rustPlatform.buildRustPackage {
-            inherit (cargoToml.package) name version;
-            src = ./.;
-            cargoLock.lockFile = ./Cargo.lock;
-          };
+        rust-toolchain = fenix-toolchain;
+        rustPlatform = pkgs.makeRustPlatform {
+          cargo = fenix-toolchain;
+          rustc = fenix-toolchain;
+        };
+      in {
+        # Rust package
+        packages.default = rustPlatform.buildRustPackage {
+          inherit (cargoToml.package) name version;
+          src = ./.;
+          cargoLock.lockFile = ./Cargo.lock;
+        };
 
-          packages.dioxus-cli = rustPlatform.buildRustPackage
-            rec {
-              pname = "dioxus-cli";
-              version = "0.4.3";
+        # TODO: refactor
+        packages.dioxus-cli =
+          rustPlatform.buildRustPackage
+          rec {
+            pname = "dioxus-cli";
+            version = "0.4.3";
 
-              src = pkgs.fetchCrate {
-                inherit pname version;
-                hash = "sha256-TWcuEobYH2xpuwB1S63HoP/WjH3zHXTnlXXvOcYIZG8=";
-              };
-
-              cargoHash = "sha256-ozbGK46uq3qXZifyTY7DDX1+vQuDJuSOJZw35vwcuxY=";
-
-              nativeBuildInputs = [ pkgs.pkg-config pkgs.cacert ];
-              buildInputs = [ pkgs.openssl ];
-
-              OPENSSL_NO_VENDOR = 1;
-
-              checkFlags = [
-                # requires network access
-                "--skip=server::web::proxy::test::add_proxy"
-                "--skip=server::web::proxy::test::add_proxy_trailing_slash"
-              ];
-
-              # Omitted: --doc
-              # Can be removed after 0.4.3 or when https://github.com/DioxusLabs/dioxus/pull/1706 is resolved
-              # Matches upstream package test CI https://github.com/DioxusLabs/dioxus/blob/544ca5559654c8490ce444c3cbd85c1bfb8479da/Makefile.toml#L94-L108
-              cargoTestFlags = [
-                "--lib"
-                "--bins"
-                "--tests"
-                "--examples"
-              ];
-
-              meta = with lib; {
-                homepage = "https://dioxuslabs.com";
-                description = "CLI tool for developing, testing, and publishing Dioxus apps";
-                license = with licenses; [ mit asl20 ];
-                maintainers = with maintainers; [ xanderio cathalmullan ];
-                mainProgram = "dx";
-              };
+            src = pkgs.fetchCrate {
+              inherit pname version;
+              hash = "sha256-TWcuEobYH2xpuwB1S63HoP/WjH3zHXTnlXXvOcYIZG8=";
             };
 
-          # Rust dev environment
-          devShells.default = pkgs.mkShell {
-            inputsFrom = [
-              config.treefmt.build.devShell
+            cargoHash = "sha256-ozbGK46uq3qXZifyTY7DDX1+vQuDJuSOJZw35vwcuxY=";
+
+            nativeBuildInputs = [pkgs.pkg-config pkgs.cacert];
+            buildInputs = [pkgs.openssl];
+
+            OPENSSL_NO_VENDOR = 1;
+
+            checkFlags = [
+              # requires network access
+              "--skip=server::web::proxy::test::add_proxy"
+              "--skip=server::web::proxy::test::add_proxy_trailing_slash"
             ];
-            shellHook = ''
-              echo
-              echo "🍎🍎 Run 'just <recipe>' to get started"
-              just
-            '';
-            buildInputs = nonRustDeps;
-            nativeBuildInputs = with pkgs; [
-              just
-              rust-toolchain
-              nixd
-              # Using locally build dioxus-cli files a strange bug with wasm-bindgen mismatch
-              # like this https://github.com/rustwasm/wasm-bindgen/discussions/3515
-              self'.packages.dioxus-cli
+
+            # Omitted: --doc
+            # Can be removed after 0.4.3 or when https://github.com/DioxusLabs/dioxus/pull/1706 is resolved
+            # Matches upstream package test CI https://github.com/DioxusLabs/dioxus/blob/544ca5559654c8490ce444c3cbd85c1bfb8479da/Makefile.toml#L94-L108
+            cargoTestFlags = [
+              "--lib"
+              "--bins"
+              "--tests"
+              "--examples"
             ];
-            RUST_BACKTRACE = 1;
-            # For rust-analyzer 'hover' tooltips to work.
-            RUST_SRC_PATH = "${fenix-channel.rust-src}/lib/rustlib/src/rust/library";
+
+            meta = with lib; {
+              homepage = "https://dioxuslabs.com";
+              description = "CLI tool for developing, testing, and publishing Dioxus apps";
+              license = with licenses; [mit asl20];
+              maintainers = with maintainers; [xanderio cathalmullan];
+              mainProgram = "dx";
+            };
           };
 
-          # Add your auto-formatters here.
-          # cf. https://numtide.github.io/treefmt/
-          treefmt.config = {
-            projectRootFile = "flake.nix";
-            programs = {
-              nixpkgs-fmt.enable = true;
-              rustfmt.enable = true;
-            };
+        # Rust dev environment
+        devShells.default = pkgs.mkShell {
+          inputsFrom = [
+            config.treefmt.build.devShell
+          ];
+          shellHook = ''
+            echo
+            echo "🍎🍎 Run 'just <recipe>' to get started"
+            just
+          '';
+          buildInputs = nonRustDeps;
+          nativeBuildInputs = with pkgs; [
+            just
+            rust-toolchain
+            nixd
+            # Using locally build dioxus-cli files a strange bug with wasm-bindgen mismatch
+            # like this https://github.com/rustwasm/wasm-bindgen/discussions/3515
+            self'.packages.dioxus-cli
+          ];
+          RUST_BACKTRACE = 1;
+          # For rust-analyzer 'hover' tooltips to work.
+          RUST_SRC_PATH = "${fenix-channel.rust-src}/lib/rustlib/src/rust/library";
+        };
+
+        # Add your auto-formatters here.
+        # cf. https://numtide.github.io/treefmt/
+        treefmt.config = {
+          projectRootFile = "flake.nix";
+          programs = {
+            alejandra.enable = true;
+            rustfmt.enable = true;
           };
         };
+      };
     };
 }
